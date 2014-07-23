d583dc59cef6a0b3b2c754df2f53bb878bc72ac4
Brian Retterer <bretterer@gmail.com>
2014-07-22 23:35:05 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+jQuery(function($) {
+
+  $('.profile_item_select').on('click', function() {
+    $('#server-response').html('<h3>Processing Backup.  You can leave this page and the backup will continue.</h3>');
+
+    var profile = $(this).data('profile');
+    console.log(profile);
+    var data = {
+      'action': 'my_action',
+      'profile': profile
+    };
+
+    // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+    $.post(ajaxurl, data, function(response) {
+      $('#server-response').html('<h3>' + response + '</h3>');
+      
+    });
+
+  });
+  
+  
+  
+  
+
+});
